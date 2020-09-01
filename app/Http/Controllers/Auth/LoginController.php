e8a4373e9b8680293cc91abe5daf797ade21874a
aditya2214 <aditokta7333@gmail.com>
2020-09-01 11:01:22 +0700

    step

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/data_reg_claim';
 
     /**
      * Create a new controller instance.
